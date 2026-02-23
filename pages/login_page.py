92a2969e04c5fd1b23e270a88024a23df03f5077
Suphakit <sb6740259117@lru.ac.th>
2026-02-23 12:58:22 +0700

    update

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,17 +1,29 @@
-# pages/login_page.py
 import streamlit as st
 import controller
 
 
 def render_login():
     st.title("🔐 เข้าสู่ระบบ")
+            
+        # -------------------------------
+    # ส่วนแสดงข้อมูลผู้จัดทำ (เรียบ ชัด ชิดซ้าย)
+    # -------------------------------
+    st.markdown("---")
 
+    st.markdown("### 👨‍💻 ผู้จัดทำ")
+
+    st.markdown("""
+    <div style="font-size:18px; line-height:1.8;">
+    <b>ชื่อ-สกุล:</b> นาย ศุภกิจ หัตภาภรณ์เนรมิต
+    <b>รหัสนักศึกษา:</b>  6740259117
+    <b>หมู่เรียน:</b> ว.6706
+    </div>
+    """, unsafe_allow_html=True)
 
     with st.form("login_form"):
         username = st.text_input("ชื่อผู้ใช้", placeholder="เช่น admin")
         password = st.text_input("รหัสผ่าน", type="password", placeholder="เช่น 1234")
         submitted = st.form_submit_button("Login")
-
 
     if submitted:
         ok, msgs, user_info = controller.login(username, password)
@@ -22,8 +34,9 @@ def render_login():
             for m in msgs:
                 st.success(m)
 
-
             st.session_state["is_logged_in"] = True
             st.session_state["user"] = user_info
             st.session_state["page"] = "books"  # หรือให้ไป borrows ก็ได้
             st.rerun()
+
+   
